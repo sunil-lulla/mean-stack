82397a048504ac8cdbb06f0634c0ea128f5f636f
Sunil-MI <sunillulla546@yahoo.com>
2017-06-05 23:48:13 +0530

    range bbased loop added

---
+++ angularjs/range-loop/controller.js
@@ -0,0 +1,30 @@
+var app = angular.module("LoopApp",[]);
+
+// app.config(["$routeProvider",function ($routeProvider) {
+// 	$routeProvider
+// 	.when('/',{
+// 		templateUrl:'login.html'
+// 	})
+// 	.when('/dashboard',{
+// 		resolve:{
+// 			"check":function ($rootScope,$location) {
+// 				if($rootScope.isLoggedIn != true){
+// 					$location.path("/");
+// 				}
+// 			}
+// 		},
+// 		templateUrl:'dashboard.html'
+// 	})
+// 	.otherwise({
+// 		redirectTo:"/"
+// 	});
+// }]);
+
+app.controller("LoopController",["$scope",function ($scope) {	
+	range = 100;
+	myRange = [];
+	for(i=0;i<range;i++)
+		myRange.push(i);
+	$scope.myRange = myRange;
+}])
+
