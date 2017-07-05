072ef36820971e16ed0d2d9b9648784878199018
Sunil-MI <sunillulla546@yahoo.com>
2017-07-05 18:50:07 +0530

    enter key code added

--- angularjs/todo/script.js
+++ angularjs/todo/script.js
@@ -2,7 +2,7 @@ var app = angular.module("todoApp",[]);
 
 app.controller("todoController",function ($scope) {
 	$scope.onSave = function(){
-		if (event.which == 13){
+		if (event.which == 13 || event.keyCode == 13){
 			$scope.addTask();
 		}
 	}
