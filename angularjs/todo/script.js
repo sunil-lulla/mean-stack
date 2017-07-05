b0a2908ba340b61a674d94a1b5f805eccd854a47
Sunil-MI <sunillulla546@yahoo.com>
2017-07-05 18:58:44 +0530

    changes in to do addTask() Fnc added

--- angularjs/todo/script.js
+++ angularjs/todo/script.js
@@ -1,12 +1,15 @@
 var app = angular.module("todoApp",[]);
 
 app.controller("todoController",function ($scope) {
+	$scope.tasks = [];
 	$scope.onSave = function(){
-		if (event.which == 13 || event.keyCode == 13){
+		if ((event.which == 13 || event.keyCode == 13) && $scope.task != ""){
 			$scope.addTask();
 		}
 	}
 	$scope.addTask = function(){
-		alert("hey There");
+		$scope.tasks.push($scope.task);
+		$scope.task = '';
+		console.log($scope.tasks); 
 	}
 })
