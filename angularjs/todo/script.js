4bce77ae822d04d5c2f6b7b12600d34f6e8f4a0b
Sunil-MI <sunillulla546@yahoo.com>
2017-07-06 13:20:57 +0530

    marked status recorded

--- angularjs/todo/script.js
+++ angularjs/todo/script.js
@@ -19,24 +19,27 @@ app.controller("todoController",function ($scope) {
 		localStorage["taskLists"] = JSON.stringify($scope.tasks);
 	}
 	$scope.editTask = function(msg){
-		console.log(msg);
 		for(i=0;i<$scope.tasks.length;i++){
 			if($scope.tasks[i].task == msg){
-				console.log("matched");
 				$scope.tasks[i].task = event.target.innerText;
 			}
 		}
-		console.log($scope.tasks);
 		localStorage["taskLists"] = JSON.stringify($scope.tasks);
 		event.target.contentEditable = event.target.contentEditable == "true"? "false":"true";
 	}
 	$scope.enterAgain = function(taskMsg){
-		// console.log("enter");
-		// console.log(taskMsg);
 		if ((event.which == 13 || event.keyCode == 13) && taskMsg != ""){
 			$scope.editTask(taskMsg);
 		}
 		
+	}
+	$scope.taskMarked = function(taskMsg){
+		for(i=0;i<$scope.tasks.length;i++){
+			if($scope.tasks[i].task == taskMsg){
+				$scope.tasks[i].status = $scope.tasks[i].status == true?true:false;
+			}
+		}
+		localStorage["taskLists"] = JSON.stringify($scope.tasks);
 	}
 
 })
