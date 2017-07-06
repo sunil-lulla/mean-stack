08f327cde6364a3d4932591df7dfe716202c02ca
Sunil-MI <sunillulla546@yahoo.com>
2017-07-06 13:12:22 +0530

    edit added to local storage

--- angularjs/todo/script.js
+++ angularjs/todo/script.js
@@ -18,14 +18,23 @@ app.controller("todoController",function ($scope) {
 		$scope.task = '';
 		localStorage["taskLists"] = JSON.stringify($scope.tasks);
 	}
-	$scope.editTask = function(){
+	$scope.editTask = function(msg){
+		console.log(msg);
+		for(i=0;i<$scope.tasks.length;i++){
+			if($scope.tasks[i].task == msg){
+				console.log("matched");
+				$scope.tasks[i].task = event.target.innerText;
+			}
+		}
+		console.log($scope.tasks);
+		localStorage["taskLists"] = JSON.stringify($scope.tasks);
 		event.target.contentEditable = event.target.contentEditable == "true"? "false":"true";
 	}
 	$scope.enterAgain = function(taskMsg){
-		console.log("enter");
-		console.log(taskMsg);
+		// console.log("enter");
+		// console.log(taskMsg);
 		if ((event.which == 13 || event.keyCode == 13) && taskMsg != ""){
-			$scope.editTask();
+			$scope.editTask(taskMsg);
 		}
 		
 	}
